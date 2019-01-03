256f9749e029293d01b3b0752179d02834d5f6bb
TheCaptaine <41933405+TheCaptaine@users.noreply.github.com>
2019-01-03 03:56:25 +0100

    Update JeuDuCent.py

--- JeuDuCent.py
+++ JeuDuCent.py
@@ -1,6 +1,24 @@
 import os
 
-chiffre, arene, passage, Game = 0, [], [], True
+""" Règle du jeu """
+
+print("Vous disposez d'une grille de 100 cases (10*10) que vous devez remplir avec un nombre par case de 1 à 100\n", "mais vous avez deux contraintes de déplacement :\n",
+	"Si vous vous déplacez en vertical ou en horizontal vous devez toujours sauter 2 cases\n", 
+	"Et si vous vous déplacez en diagonale, vous devez toujours sauter 1 case.\n")
+
+print("Exemple de déplacement possible :\n") 
+print("-----".join(" "*(5)), "\n", end = '')
+print("     ".join("|"*(5)))
+print("  1--", "-----".join(" "*(3)), "->2  ")
+print("     ".join("|"*(5)))
+print("-----".join(" "*(5)), "\n")
+
+input("< Appuier sur entrer pour commencer le jeu. ")
+
+""" Jeux """
+
+chiffre, arene, passage, deplacementL, Game = 0, [], [], [(ligne-2, colonne-2), (ligne-2, colonne+2), (ligne+2, colonne-2), (ligne+2, colonne+2),
+(ligne, colonne-3), (ligne, colonne+3), (ligne-3, colonne), (ligne+3, colonne)], True
 for i in range(10):
 	for k in range(10):
 		passage.append(' ')
@@ -36,8 +54,7 @@ def verification1(ligne, colonne):
 			if arene[l][c] == str(chiffre - 1):
 				lgne = l
 				clnne = c
-	if (ligne, colonne) in [(lgne,clnne-3), (lgne,clnne+3), (lgne-2,clnne-2), 
-	(lgne-2,clnne+2), (lgne+2,clnne-2), (lgne+2,clnne+2),(lgne-3,clnne), (lgne+3,clnne)]:
+	if (ligne, colonne) in deplacementL:
 		try:
 			if arene[ligne][colonne] != " ":
 				print("\nDeplacement impossible\n")
@@ -53,8 +70,7 @@ def verification1(ligne, colonne):
 
 def verification2(ligne, colonne, chance=0):
 	global chiffre
-	for k in [(ligne-2, colonne-2), (ligne-2, colonne+2), (ligne+2, colonne-2), (ligne+2, colonne+2),
-(ligne, colonne-3), (ligne, colonne+3), (ligne-3, colonne), (ligne+3, colonne)]:
+	for k in deplacementL:
 		try:
 			if arene[k[0]][k[1]] == " " and k[0] >= 0 and k[1] >= 0:
 				print('{}, {}'.format(k[0], k[1]))
@@ -81,8 +97,7 @@ while Game:
 					draw()
 					Game = False
 		else:
-			print("\n/{} Nombre positif requis\n".format(chr(92)))
+			print("\n/!{} Nombre positif requis\n".format(chr(92)))
 	except ValueError:
 		os.system('clear')
 		print("\nVeuillez choisir des nombres entier compris entre [0, 9]")
-
