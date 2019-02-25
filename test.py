b075f445fa83d6dec55d4447b81b1265a0234727
TheCaptaine <41933405+TheCaptaine@users.noreply.github.com>
2019-02-25 16:20:11 +0100

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@
 
 import matplotlib.pyplot as plt 
 import numpy as np 
-from scipy.optimize import fmin, fsolve
+import scipy.integrate as integration
 
 longueur_onde = 0.5e-3
 D = 10
@@ -12,28 +12,54 @@ d = 1
 s = 10
 
 #Q1
-difference_de_marche = lambda X, Y, d, D, ys, s : np.sqrt(X**2+(Y-d/2)**2+D**2)+np.sqrt((ys-d/2)**2+s**2)-np.sqrt(X**2+(Y+d/2)**2+D**2)-np.sqrt((ys+d/2)**2+s**2)
+difference_de_marche = lambda ys, X : np.sqrt(X**2+(Y-d/2)**2+D**2)+np.sqrt((ys-d/2)**2+s**2)-np.sqrt(X**2+(Y+d/2)**2+D**2)-np.sqrt((ys+d/2)**2+s**2)
 
 I = lambda x : 4*np.cos(np.pi*x/longueur_onde)**2
 Y = np.linspace(-0.01, 0.01, 1000)
 
 
-plt.plot(Y, I(difference_de_marche(0, Y, d, D, 0, 10)), 'r-', label='X=0')
-plt.plot(Y, I(difference_de_marche(10, Y, d, D, 0, 10)), 'b-', label='X=10')
+plt.plot(Y, I(difference_de_marche(0, 0)), 'r-', label='X=0')
+plt.plot(Y, I(difference_de_marche(0, 10)), 'b-', label='X=10')
 plt.xlabel('Y/mm')
 plt.ylabel('Intensity/a.u')
-plt.title('I(X,Y,)')
+plt.title('I(X,Y,ys)')
 plt.legend(loc='upper right')
 plt.xticks(np.arange(-0.01, 0.015, 0.005))
 plt.axis([-0.01, 0.01, 0, 4])
 plt.show()
 
-plt.plot(Y, I(difference_de_marche(0, Y, d, D, 0, 10)), 'r-', label='X=0')
-plt.plot(Y, I(difference_de_marche(0, Y, d, D, 0.001, 10)), 'k-', label='X=0.001')
+plt.plot(Y, I(difference_de_marche(0, 0)), 'r-', label='X=0')
+plt.plot(Y, I(difference_de_marche(0.001, 0)), 'k-', label='X=0.001')
 plt.legend(loc='upper right')
 plt.xticks(np.arange(-0.01, 0.015, 0.005))
 plt.xlabel('Y/mm')
 plt.ylabel('Intensity/a.u')
 plt.axis([-0.01, 0.01, 0, 4])
 plt.show()
+
+#Q2
+def intensity(ys, X, Y, R, d, D, s, longueur_onde):
+    return 4/R*np.cos(np.pi*(np.sqrt(X**2+(Y-d/2)**2+D**2)+np.sqrt((ys-d/2)**2+s**2)-np.sqrt(X**2+(Y+d/2)**2+D**2)-np.sqrt((ys+d/2)**2+s**2))/longueur_onde)**2
+def expint(X, Y, R, d, D, s, longueur_onde):
+    return integration.quad(intensity, -R/2, R/2, args=(X, Y, R, d, D, s, longueur_onde))[0]
+
+#Q3
+vec_expint = np.vectorize(expint)
+plt.plot(Y, vec_expint(0, Y, 1e-6, d, D, s, longueur_onde), 'r-', label='R=1e-6')
+plt.plot(Y, vec_expint(0, Y, 2e-3, d, D, s, longueur_onde), 'b-', label='R=2e-3')
+plt.plot(Y, vec_expint(0, Y, 3e-3, d, D, s, longueur_onde), 'k-', label='R=3e-3')
+plt.plot(Y, vec_expint(0, Y, 4e-3, d, D, s, longueur_onde), 'g-', label='R=4e-3')
+plt.xlabel('Y/mm')
+plt.ylabel('Intensity/a.u')
+plt.title('I(X,Y,R)')
+plt.legend(loc='upper right')
+plt.xticks(np.arange(-0.01, 0.015, 0.005))
+plt.axis([-0.01, 0.01, 0, 4])
+plt.show()
+
+#Q4
+Y = np.linspace(-0.01, 0.01, 1000)
+X = np.linspace(-10, 10, 1000)
+XX, YY = np.meshgrid(X, Y)
+plt.imshow(vec_expint(XX, YY, 1e-6, d, D, s, longueur_onde))
 
